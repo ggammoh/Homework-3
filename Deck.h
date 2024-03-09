fe0fcb28d6a43a8ee61caf619aaaa4f9aaee3494
Kanav Shetty <shettykv@mail.uc.edu>
2024-03-08 20:46:14 -0500

    updated changes

--- Deck.h
+++ Deck.h
@@ -9,11 +9,47 @@ class Deck{
         Card *bottom; //Pointer to the bottom of the deck
 
     public:
-        Deck();
-        int Pull(); //Pull the card from the top of the deck
-        void Add(int cardNum);  //Add the won cards to the bottom of the deck
-        int getNumCards(); //Returns the number of cards the player has left
-        bool isEmpty(); //Return wether or not the player has any card left
-        void printDeck();
-        void printTop();
+        Deck(){
+            numCards = 0;
+        }
+        int Pull(){ //Pull the card from the top of the deck
+            Card *temp = top;
+            int val = temp->value;
+
+            top=top->next;
+
+            delete temp;
+            numCards--;
+
+            return val;
+        }
+        void Add(int cardNum){//Add the won cards to the bottom of the deck
+            Card* temp = new Card(cardNum);
+            if(isEmpty()){
+                top = bottom = temp;
+                numCards++;
+                return;
+            }
+            bottom->next = temp;
+            bottom = temp;
+            numCards++;
+        }  
+        int getNumCards(){ //Returns the number of cards the player has left
+            return numCards;
+        }
+        bool isEmpty(){ //Return wether or not the player has any card left
+            return numCards<=0;
+        }
+        void printDeck(){
+            Card *temp = top;
+            while (temp != nullptr) {
+            std::cout << temp->value << " ";
+            temp = temp->next;
+        }
+        std::cout << "\n";
+        }
+        void printTop(){
+            int rtnval = top->value;
+            std::cout << rtnval;
+        }
 };
--- DeckTest.cpp
+++ DeckTest.cpp
@@ -1,20 +1,20 @@
-// #include "Deck.h"
-// int main(){
-//     Deck myDeck;
+#include "Deck.h"
+int main(){
+    Deck myDeck;
 
-//     myDeck.Add(1);
-//     myDeck.Add(10);
-//     myDeck.Add(13);
-//     myDeck.Add(2);
-//     myDeck.Add(12);
-//     myDeck.Add(11);
-//     myDeck.Add(5);
-//     myDeck.printDeck();
-//     std::cout << myDeck.Pull();
-//     myDeck.printDeck();
-//     std::cout << myDeck.Pull();
-//     myDeck.printDeck();
-//     std::cout << myDeck.Pull();
-//     myDeck.printDeck();
+    myDeck.Add(1);
+    myDeck.Add(10);
+    myDeck.Add(13);
+    myDeck.Add(2);
+    myDeck.Add(12);
+    myDeck.Add(11);
+    myDeck.Add(5);
+    myDeck.printDeck();
+    std::cout << myDeck.Pull() << std::endl;
+    myDeck.printDeck();
+    std::cout << myDeck.Pull()<< std::endl;
+    myDeck.printDeck();
+    std::cout << myDeck.Pull()<< std::endl;
+    myDeck.printDeck();
     
-// }
+ }
